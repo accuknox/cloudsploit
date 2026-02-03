f996fbb87bedd23cd15ee6c9b0eb7b1af0184d54
Chirag-CG <impochigyl@gmail.com>
2026-02-03 09:43:39 +0530

    aws mx-central fix

--- helpers/aws/regions.js
+++ helpers/aws/regions.js
@@ -213,4 +213,3 @@ module.exports = {
     opensearchserverless: ['us-east-2', 'us-east-1', 'us-west-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1',
         'eu-central-1', 'eu-west-1']
 };
-
